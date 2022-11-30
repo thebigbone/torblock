555445c4def83eb1f1813b0cb51e3d544f37a367
thebigbone <pacman@duck.com>
2022-12-01 00:01:32 +0530

    used bine/tor library for now

---
+++ go.mod
@@ -0,0 +1,13 @@
+module tor
+
+go 1.19
+
+require (
+	github.com/cretz/bine v0.2.0
+	golang.org/x/net v0.2.0
+)
+
+require (
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/sys v0.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"time"
+
+	"github.com/cretz/bine/tor"
+	"golang.org/x/net/html"
 )
 
 func main() {
@@ -14,7 +21,34 @@ func main() {
 
 	if response != 200 {
 		fmt.Println("something went wrong.")
-		// do stuff
+	} else {
+		fmt.Printf("Starting tor and fetching title of %s, please wait a few seconds...", *url)
+		t, err := tor.Start(nil, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer t.Close()
+		// Wait at most a minute to start network and get
+		dialCtx, dialCancel := context.WithTimeout(context.Background(), time.Minute)
+		defer dialCancel()
+		// Make connection
+		dialer, err := t.Dialer(dialCtx, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		httpClient := &http.Client{Transport: &http.Transport{DialContext: dialer.DialContext}}
+		// Get /
+		resp, err := httpClient.Get(*url)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer resp.Body.Close()
+		// Grab the <title>
+		parsed, err := html.Parse(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("Title: %v\n", getTitle(parsed))
 	}
 
 	fmt.Println("site url: ", *url)
@@ -26,4 +60,20 @@ func siteresponse(url string) int {
 		log.Fatal(err)
 	}
 	return resp.StatusCode
+}
+
+func getTitle(n *html.Node) string {
+	if n.Type == html.ElementNode && n.Data == "title" {
+		var title bytes.Buffer
+		if err := html.Render(&title, n.FirstChild); err != nil {
+			panic(err)
+		}
+		return strings.TrimSpace(title.String())
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if title := getTitle(c); title != "" {
+			return title
+		}
+	}
+	return ""
 }
